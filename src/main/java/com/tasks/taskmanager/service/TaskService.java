ece7b85d343eca4edbd5ce9071a7cc588afff184
Tfactor2020@gmail.com <Tfactor08>
2025-02-12 22:01:31 +0300

    add TaskService with basic methods

---
+++ src/main/java/com/tasks/taskmanager/service/TaskService.java
@@ -0,0 +1,22 @@
+package com.tasks.taskmanager.service;
+
+import com.tasks.taskmanager.model.Task;
+import com.tasks.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
