240373f599241990407143d378463fcbc37948b5
Tfactor2020@gmail.com <Tfactor08>
2025-02-12 22:07:03 +0300

    add TaskController with basic methods

---
+++ src/main/java/com/tasks/taskmanager/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.tasks.taskmanager.controller;
+
+import com.tasks.taskmanager.service.TaskService;
+import com.tasks.taskmanager.model.Task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.saveTask(task);
+    }
+}
