821127534571451ca7c155cccb14586cf12006b5
Tfactor2020@gmail.com <Tfactor08>
2025-02-13 12:18:20 +0300

    add methods for updating and deleting in the service layer

--- src/main/java/com/tasks/taskmanager/service/TaskService.java
+++ src/main/java/com/tasks/taskmanager/service/TaskService.java
@@ -2,6 +2,8 @@ package com.tasks.taskmanager.service;
 
 import com.tasks.taskmanager.model.Task;
 import com.tasks.taskmanager.repository.TaskRepository;
+import com.tasks.taskmanager.exception.TaskNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,24 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    public Task saveTask(Task task) {
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+            .orElseThrow(() -> new TaskNotFoundException("Taks not found with id: " + id));
+    }
+
+    public Task createTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task taskDetails) {
+        Task task = getTaskById(id);
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.getCompleted());
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
     }
 }
