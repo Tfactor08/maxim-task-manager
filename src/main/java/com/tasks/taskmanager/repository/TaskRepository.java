26045035b9fb41e3f7217a0454977251316f6513
Tfactor2020@gmail.com <Tfactor08>
2025-02-13 22:04:32 +0300

    add methods for filtering tasks in the repository layer

--- src/main/java/com/tasks/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/tasks/taskmanager/repository/TaskRepository.java
@@ -3,6 +3,12 @@ package com.tasks.taskmanager.repository;
 import com.tasks.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.List;
+import java.time.LocalDate;
 
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCreatedAt(LocalDate date);
+    List<Task> findByCreatedAtAndCompleted(LocalDate date, boolean completed);
+    List<Task> findByCreatedAtBetween(LocalDate startDate, LocalDate endDate);
+    List<Task> findByCreatedAtBetweenAndCompleted(LocalDate startDate, LocalDate endDate, boolean completed);
 }
