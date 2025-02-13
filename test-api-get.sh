80ca2a05f12482395ca88c81de2d9698767b59e2
Tfactor2020@gmail.com <Tfactor08>
2025-02-13 12:49:42 +0300

    add getting by id feature to the get script

--- test-api-get.sh
+++ test-api-get.sh
@@ -1,1 +1,5 @@
-printf "$(curl -X GET localhost:8080/tasks --silent --show-error)\n"
+if [ $# -eq 0 ]; then
+    printf "$(curl -X GET localhost:8080/tasks --silent --show-error)\n"
+else
+    printf "$(curl -X GET localhost:8080/tasks/$1 --silent --show-error)\n"
+fi
