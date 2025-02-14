417ece784ca299f72a8703e587ca139069c47b94
Tfactor2020@gmail.com <Tfactor08>
2025-02-14 23:14:45 +0300

    add repository testing class; add test for findByCreatedAt repository method

---
+++ src/test/java/com/tasks/taskmanager/repository/TaskRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.tasks.taskmanager.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+import com.tasks.taskmanager.model.Task;
+import com.tasks.taskmanager.repository.TaskRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@ActiveProfiles("test")
+public class TaskRepositoryTest {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    public void TaskRepository_FindByCreatedAt_ReturnsFoundTask() {
+        Task task = new Task(); task.setTitle("task1");
+        LocalDate now = LocalDate.now();
+
+        taskRepository.save(task);
+        Task foundTasks = taskRepository.findByCreatedAt(now).get(0);
+
+        Assertions.assertThat(foundTasks).isEqualTo(task);
+    }
+}
